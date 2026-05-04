547a152cfd80bf00e6add580efac1f08583e63a5
kynaruniverse <kynaruniverse@gmail.com>
2026-05-04 18:23:33 +0100

    update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
 import GameManager from "./components/GameManager";
 
 /**
